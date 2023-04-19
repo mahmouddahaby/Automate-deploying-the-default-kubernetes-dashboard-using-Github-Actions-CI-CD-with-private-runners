1201a1fe24aeb13c17c429477314e060c1e726b5
mahmouddahaby <mahmoud.dahaby7@gmail.com>
2023-04-19 15:03:56 +0200

    First touch

---
+++ sa/main.tf
@@ -0,0 +1,29 @@
+resource "google_service_account" "instance-sa" {
+  account_id = "instance-sa"
+  display_name = "instance-sa"
+}
+resource "google_service_account" "custom_gke_sa" {
+  account_id   = "custom-gke-sa"
+  display_name = "Custom GKE Service Account"
+}
+
+resource "google_project_iam_member" "instance" {
+  project = var.project
+  role = "roles/container.admin"
+  member = "serviceAccount:${google_service_account.instance-sa.email}"
+}
+resource "google_project_iam_member" "gke_sa" {
+  role    = "roles/container.admin"
+  member  = "serviceAccount:${google_service_account.custom_gke_sa.email}"
+  project = var.project
+}
+resource "google_project_iam_member" "gke_storage_sa" {
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.custom_gke_sa.email}"
+  project = var.project
+}
+resource "google_project_iam_member" "gke_object_sa" {
+  role    = "roles/storage.objectAdmin"
+  member  = "serviceAccount:${google_service_account.custom_gke_sa.email}"
+  project = var.project
+}
